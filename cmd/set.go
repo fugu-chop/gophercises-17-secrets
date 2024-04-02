20244dddbb2e02f74c5c2ffe1c4259530b495f56
Dean Wan <deanwan14@gmail.com>
2024-04-02 21:04:14 +1100

    feat: encrypt function

--- cmd/set.go
+++ cmd/set.go
@@ -1,10 +1,12 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/hex"
 	"fmt"
+	"io"
 
 	"github.com/spf13/cobra"
 )
@@ -32,4 +34,25 @@ func init() {
 	setCmd.Flags().StringP("encryptionKey", "k", "", `The keyName flag (-k) is 
 	used to provide an encryption key to encrypt a key`)
 	setCmd.MarkFlagRequired("encryptionKey")
+}
+
+func encryptKey(value, encryptionKey string) error {
+	key, _ := hex.DecodeString(encryptionKey)
+	plaintext := []byte(value)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return fmt.Errorf("failed to create cipher: %s", err)
+	}
+
+	// Generate a unique (but not necessarily secure) IV
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return fmt.Errorf("failed to generate ciphertext: %s", err)
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	return nil
 }
