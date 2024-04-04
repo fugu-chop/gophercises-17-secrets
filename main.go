03196331992ff8cffbabd2f3d27e6678ab64f431
Dean Wan <deanwan14@gmail.com>
2024-04-04 20:22:55 +1100

    feat: initialise secrets file on open

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "secrets/cmd"
+import (
+	"fmt"
+	"log"
+	"os"
+	vault "secrets/pkg/vault"
+)
 
 func main() {
-	cmd.Execute()
+	// Temporarily disable CLI
+	// cmd.Execute()
+	vault := vault.FileVault{
+		EncryptionKey: "6368616e676520746869732070617373",
+		FilePath:      "/Users/dean/Desktop",
+	}
+
+	var secretsFile *os.File
+	// Check if secrets files exists
+	if _, err := os.Stat(vault.FilePath + "/secrets.txt"); err != nil {
+		log.Printf("creating secrets.txt file at %s", vault.FilePath)
+		secretsFile, err = os.Create(vault.FilePath + "/secrets.txt")
+		if err != nil {
+			log.Fatalf("could not create secrets file: %s", err)
+		}
+	} else {
+		secretsFile, err = os.Open(vault.FilePath + "/secrets.txt")
+		if err != nil {
+			log.Fatalf("could not open secrets file: %s", err)
+		}
+	}
+
+	fmt.Println(secretsFile.Name())
 }
